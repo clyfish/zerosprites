38993006bdb34474f1456188b753976d5d56c126
Cheng Liangyu <clyfish@gmail.com>
2013-09-24 21:41:52 +0800

    replace blobb with rectpack

--- sprites.sh
+++ sprites.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-BASE_DIR=$(cd `dirname $0` && pwd)
+RECTPACK_TIMEOUT=20
 
-[[ $# -eq 0 ]] && {
-    echo "Usage: $0 (directory | png files)"
+BASE_DIR=`dirname $0`
+
+[[ $# -le 1 || ! "$1" =~ ^[0-9]+$ ]] && {
+    echo "Usage: $0 padding (directory | png files)"
     echo "Output consists of sprites.txt and sprites.png"
     exit 1
 }
+
+padding=$1
+shift
 
 which identify &>/dev/null && which convert &>/dev/null || {
     echo "Please install ImageMagick"
@@ -22,23 +27,26 @@ cd "$tmpdir"
 
 for png in $files
 do
-    echo "$png	`identify -format "%w %h" ../"$png"`"
-done > image_size.txt
-n=`wc -l < image_size.txt`
+    echo "$png	`identify -format "%w %h" ../"$png" | awk -v padding="$padding" '{print $1+padding, $2+padding}'`"
+done > input.txt
+n=`wc -l < input.txt`
 
-if [[ $n -le 8 ]]
+(
+    ulimit -t "$RECTPACK_TIMEOUT"
+    $BASE_DIR/rectpack -b0 -i `awk '{if(NR!=1){printf ","}printf "%s",$2"x"$3}' input.txt` > output_rectpack.txt
+)
+
+if [[ `wc -l < output_rectpack.txt` -eq $((n+1)) ]]
 then
-    awk 'BEGIN{print '"$n"'}{print $2+1,$3+1}' image_size.txt > input_blobb.txt
-    $BASE_DIR/blobb input_blobb.txt output_blobb.txt -n -fx -o &>/dev/null
-    w=`awk 'NR==1{print int($1)-1}' output_blobb.txt`
-    h=`awk 'NR==2{print int($1)-1}' output_blobb.txt`
-    paste <(awk '{print $1}' image_size.txt) <(awk 'NR>='"$n"'+5 && NF==2{print int($1),int($2)}' output_blobb.txt) > ../sprites.txt
+    w=`awk -v padding="$padding" 'NR==1{print $1-padding}' output_rectpack.txt`
+    h=`awk -v padding="$padding" 'NR==1{print $2-padding}' output_rectpack.txt`
+    paste <(awk '{print $2, $3, $1, NR}' input.txt | sort) <(awk -F'[x(), ]+' 'NR>1{print $1, $2, $3, $4}' output_rectpack.txt | sort) | sort -n -k4 | awk '{print $3, $7, $8}' > ../sprites.txt
 else
-    awk '{print NR,$2+1,$3+1}' image_size.txt > input_btree.txt
+    awk '{print NR,$2,$3}' input.txt > input_btree.txt
     $BASE_DIR/btree input_btree.txt -simple -times 200 -maxIte 99999999 &>/dev/null
     w=`awk 'NR==2{print $3-1}' input_btree.txt.info`
     h=`awk 'NR==3{print $3-1}' input_btree.txt.info`
-    paste <(awk '{print $1}' image_size.txt) <(awk 'NF==5{print $2,$4}' input_btree.txt.info) > ../sprites.txt
+    paste <(awk '{print $1}' input.txt) <(awk 'NF==5{print $2,$4}' input_btree.txt.info) > ../sprites.txt
 fi
 
 cd ..
