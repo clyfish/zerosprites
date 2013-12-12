fbb2ab7c478f37a7192340f1ea53cdceca228e78
Cheng Liangyu <clyfish@gmail.com>
2013-12-12 19:44:55 +0800

    sort by filename when dimensions are same

--- sprites.sh
+++ sprites.sh
@@ -40,16 +40,18 @@ if [[ `wc -l < output_rectpack.txt` -eq $((n+1)) ]]
 then
     w=`awk -v padding="$padding" 'NR==1{print $1-padding}' output_rectpack.txt`
     h=`awk -v padding="$padding" 'NR==1{print $2-padding}' output_rectpack.txt`
-    paste <(awk '{print $2, $3, $1, NR}' input.txt | sort) <(awk -F'[x(), ]+' 'NR>1{print $1, $2, $3, $4}' output_rectpack.txt | sort) | sort -n -k4 | awk '{print $3, $7, $8}' > ../sprites.txt
+    awk -F'[x(), ]+' 'NR>1{print $1, $2, $3, $4}' output_rectpack.txt > output.txt
 else
     awk '{print NR,$2,$3}' input.txt > input_btree.txt
     $BASE_DIR/btree input_btree.txt -simple -times 200 -maxIte 99999999 &>/dev/null
     w=`awk -v padding="$padding" 'NR==2{print $3-padding}' input_btree.txt.info`
     h=`awk -v padding="$padding" 'NR==3{print $3-padding}' input_btree.txt.info`
-    paste <(awk '{print $1}' input.txt) <(awk 'NF==5{print $2,$4}' input_btree.txt.info) > ../sprites.txt
+    paste <(awk '{print $2,$3}' input_btree.txt) <(awk 'NF==5{print $2,$4}' input_btree.txt.info) > output.txt
 fi
 
+paste <(awk '{print $2, $3, $1}' input.txt | sort) <(sort output.txt -n -k1 -k2 -k4 -k3) | awk '{print $3, $6, $7}' | sort > ../sprites.txt
+
 cd ..
-rm -r "$tmpdir"
+#rm -r "$tmpdir"
 
 convert -strip -size "${w}x${h}" xc:"rgba(0,0,0,0)" `awk '{print $1" -geometry +"$2"+"$3" -composite"}' sprites.txt` sprites.png
